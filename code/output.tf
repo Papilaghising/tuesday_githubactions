a168756c942288e5121780f6cd202e7a9b5e7593
pailaghising <papilaghising1111@gmail.com>
2024-05-28 16:34:59 +0545

    chnaged workflow

--- code/output.tf
+++ code/output.tf
@@ -2,7 +2,3 @@ output "public_ip" {
   description = "public_ip of the ec2 instance"
   value       = aws_instance.ec2_instance.public_ip
 }
-output "bucket_id" {
-  description = "public_ip of the ec2 instance"
-  value       = aws_s3_bucket.papila.id
-}
