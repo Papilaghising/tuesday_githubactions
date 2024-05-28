6e44ac2133c18840c6ca9566cc39aef067b8eb7c
pailaghising <papilaghising1111@gmail.com>
2024-05-28 16:23:12 +0545

    chnaged credentials

--- code/versions.tf
+++ code/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.6" # which means any version equal & above
+      version = ">= 5.6" # which means any version equal & 
     }
   }
   required_version = ">= 0.13"
