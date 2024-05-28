c72434e3e0f8567e144572f96949f633cec3e318
pailaghising <papilaghising1111@gmail.com>
2024-05-28 15:49:17 +0545

    edited workflows and provider.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "ec2_instance" {
-  #count                       = var.number_of_instances
   ami                         = var.ami_id
   subnet_id                   = var.subnet_id
   instance_type               = var.instance_type
